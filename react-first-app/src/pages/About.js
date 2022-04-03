9bf1eae4947f6206dc2c1b67543877b19c77aef5
nicolas <nicolas.rossat@hotmail.fr>
2022-04-03 17:08:36 +0200

    Ajout projet complet tuto React

---
+++ react-first-app/src/pages/About.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Logo from '../components/Logo';
+import Navigation from '../components/Navigation';
+
+const About = () => {
+    return (
+        <div>
+            <Logo />
+            <Navigation />
+            <h1>ABOUT</h1>
+            <br />
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Corrupti ipsam accusamus vero fugiat pariatur officia quibusdam eveniet eligendi? Esse natus doloremque, quo incidunt itaque repellat, vero eius, similique vitae tenetur nihil sit. Suscipit molestiae fugit laudantium adipisci sunt explicabo a, debitis recusandae perferendis quis ducimus assumenda aspernatur aliquid ea quae eius harum ex saepe exercitationem autem id! Quisquam, rem? Quisquam cumque ducimus nesciunt corporis pariatur natus placeat, fuga necessitatibus aperiam consequuntur eum totam delectus quibusdam est labore quam animi? Quidem nisi, porro commodi accusantium laboriosam quia, dolor expedita voluptatibus error molestias, facilis maxime! Dicta, rerum consectetur et tempore quibusdam eius?</p>
+            <br />
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Commodi quas impedit ducimus asperiores a iure nobis ipsam beatae dolorem aliquam. Aliquam, nulla autem? Error vel corrupti velit, libero molestiae iusto! Veniam provident, tenetur voluptates error sequi nulla eveniet ea voluptate ipsum incidunt cum. A repudiandae sequi tempore? Ratione eaque provident sit neque cumque quod pariatur qui minus molestiae aliquid autem, quae non dolor illum ducimus doloremque dicta, sint quo. Suscipit, culpa, asperiores labore libero nisi ab voluptates fuga hic repellendus autem, ullam atque. Tenetur voluptatem reprehenderit autem omnis saepe magni rem cum ab, illum neque fugit quidem aut molestias atque corrupti! A molestias nulla earum fugiat consequuntur expedita enim molestiae reprehenderit, explicabo adipisci laboriosam quam unde, repellat fuga vitae voluptatibus repellendus. Placeat quod in, consectetur ipsam, nulla quae temporibus autem nostrum eos porro suscipit debitis quasi dolore quo asperiores rem provident at, perspiciatis est deleniti ea natus veritatis. Aliquam nemo porro odio. Beatae obcaecati necessitatibus a alias! Corrupti impedit a, molestiae aspernatur facere labore! Iure incidunt porro id ad, velit fugiat minus temporibus dolores et. Nemo odit rerum ipsum tempore necessitatibus natus eaque repellat, assumenda magni ratione porro corrupti quibusdam voluptatibus quisquam adipisci amet numquam distinctio, saepe, a illum fugiat.</p>
+        </div>
+    );
+};
+
+export default About;
